6ba853abfe4e3b629729d8d1c73f8eea81f1c49f
Chas <Chas@Fedora>
2025-04-01 02:14:05 -0700

    improved

---
+++ .bashrc.d/aliases.sh
@@ -0,0 +1,9 @@
+alias dotfiles="cd ~/.dotfiles/"
+
+alias bashrc="nvim ~/.bashrc.d/"
+alias sourcebash="source ~/.bashrc.d/aliases.sh ~/.bashrc.d/functions.sh"
+
+alias ll="ls -lah"
+alias lld="ls -lahd */"
+
+alias rd="rm -r"
