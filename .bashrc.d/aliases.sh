0513ecff128752a556f47eaf27d457adb07810b2
Chas <chas@fedora>
2025-04-10 18:10:43 -0700

    upgraded prompt and alacritty and how-to.txt

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -3,6 +3,7 @@ alias dotfiles="cd ~/.dotfiles/"
 alias bashrc="nvim ~/.bashrc.d/"
 alias sourcebash="source ~/.bashrc.d/aliases.sh ~/.bashrc.d/functions.sh"
 
+alias ls="ls --color=auto"
 alias ll="ls -lah"
 alias lld="ls -lahd */"
 
--- .bashrc.d/prompt.sh
+++ .bashrc.d/prompt.sh
@@ -1,1 +1,2 @@
-PROMPT_COMMAND='PS1_CMD1=$(git branch --show-current 2>/dev/null)'; PS1='\u: \W:${PS1_CMD1} \$ '
+PROMPT_COMMAND='PS1_CMD1=$(__git_ps1 " (%s)")'
+PS1='\[\e[91m\]\u\[\e[0m\]@\[\e[96m\]\h\[\e[0m\] \[\e[32m\]\W\[\e[33m\]${PS1_CMD1}\[\e[0m\] \$ '
